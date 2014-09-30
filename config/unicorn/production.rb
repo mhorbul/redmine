8904eb768430218f896cb14c997962385eeb820c
Max Horbul <max@gorbul.net>
2014-09-29 21:07:44 -0700

    add unicorn config file

---
+++ config/unicorn/production.rb
@@ -0,0 +1,16 @@
+root = File.expand_path(File.join(File.dirname(__FILE__), "..", "..")
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+# Port configuration
+listen ENV['PORT']
+worker_processes 2
+timeout 30
+
+# Force the bundler gemfile environment variable to
+# reference the capistrano "current" symlink
+before_exec do |_|
+  ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
+end
